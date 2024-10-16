d45d28cbfe10b5be9e0459010f51b6cdc2c8c095
TundyT <tundyt@duck.com>
2024-10-16 16:24:31 +0200

    add counting chars (the cooler way)

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import string
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    chars_dict = get_count_chars(text)
     print(f"{num_words} words found in this document")
+    print(chars_dict)
 
 def get_num_words(text):
     words = text.split()
@@ -11,5 +15,13 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_count_chars(text):
+    chars_dict = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in string.ascii_lowercase:
+            chars_dict[lowered] = chars_dict.get(lowered, 0) + 1
+    return chars_dict
 
 main()
